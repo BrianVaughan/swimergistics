7022e87f68360837b7a6c533fa0d54c0a36c91e3
Brian Vaughan <bvaughan64@gmail.com>
2014-03-12 19:06:08 -0600

    reformat code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('swimergisticsApp')
-  .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope) {
 
-    $scope.setBackground=function(url) {
-        $scope.urlbackimage=url;
-    }
+        $scope.setBackground = function (url) {
+            $scope.urlbackimage = url;
+        }
         $scope.setBackground("../images/swimming_pool_1.jpg");
-  });
+    });
