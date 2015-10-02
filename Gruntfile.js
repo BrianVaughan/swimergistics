7269cc4a1a006dbb956d570938762b40c555db05
vaughanbd <briandouglas.vaughan@familysearch.org>
2015-10-02 16:31:25 -0600

    removed change image names

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,7 @@ var pkg = require('./package.json');
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          //  '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
