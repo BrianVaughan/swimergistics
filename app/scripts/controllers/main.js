8a1381b5f2170bdf322848862897e354e450f143
vaughanbd <briandouglas.vaughan@familysearch.org>
2015-10-03 22:33:59 -0600

    adding image scopes because problem with moving out to dist

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,11 @@
 
 angular.module('swimergisticsApp')
     .controller('MainCtrl', function ($scope) {
-
+        $scope.mainswimimage='Swimming_pool_1';
+        $scope.titleimage='SwimergisticTitle1'; 
+        $scope.logo='logo4';
         $scope.setBackground = function (url) {
             $scope.urlbackimage = url;
         }
-        $scope.setBackground("../images/Swimming_pool_1.jpg");
+        $scope.setBackground("../images/{{mainswimimage}}.jpg");
     });
