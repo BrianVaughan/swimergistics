de5c27b0c46c2569bbe873e44d75cdf370df8a4f
vaughanbd <briandouglas.vaughan@familysearch.org>
2015-10-03 23:13:09 -0600

    adding a delay

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,14 @@
 
 angular.module('swimergisticsApp')
     .controller('MainCtrl', function ($scope) {
+    	$timeout(function () {
         $scope.mainswimimage='Swimming_pool_1';
         $scope.titleimage='SwimergisticTitle1'; 
         $scope.logo='logo4';
+
         $scope.setBackground = function (url) {
             $scope.urlbackimage = url;
         }
         $scope.setBackground("../images/{{mainswimimage}}.jpg");
+        }, 1000);
     });
